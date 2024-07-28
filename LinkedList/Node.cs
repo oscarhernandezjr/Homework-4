0fe775a293761cfa56418d0d6a003a9e164e3d83
Oscar Hernandez <oscar_51293@hotmail.com>
2024-07-28 01:27:40 -0700

    update

---
+++ LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Homework_4.LinkedList
+{
+	public class Node
+	{
+
+		public int Data { get; set; }
+		public Node? Next { get; set; }
+
+		public void DisplayNode()
+		{
+			Console.WriteLine(Data);
+		}
+
+	}
+	
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Linked-List
+
+using Homework_4.LinkedList;
+
+LinkedList LinkedList = new LinkedList();
+LinkedList.InsertFirst(1);
+LinkedList.InsertFirst(2);
+LinkedList.InsertFirst(3);
+LinkedList.InsertFirst(4);
+
+LinkedList.DeleteFirst();
+LinkedList.DeleteFirst();
+
+LinkedList.InsertLast(546);
+LinkedList.InsertLast(3434);
+
+LinkedList.DisplayList();
